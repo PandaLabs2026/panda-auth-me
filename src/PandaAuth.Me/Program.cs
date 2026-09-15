049487ad10003c295f6e8fe2b6f7c2cf8701bdcd
Yuhooo <2512541+jiayuhu@users.noreply.github.com>
2026-09-16 01:32:03 +0800

    fix(me): register client signing key

--- src/PandaAuth.Me/Program.cs
+++ src/PandaAuth.Me/Program.cs
@@ -37,6 +37,7 @@ builder.Services.AddOpenIddict()
         options.AllowAuthorizationCodeFlow();
         options.AllowRefreshTokenFlow();
         options.AddEphemeralEncryptionKey();
+        options.AddEphemeralSigningKey();
         options.UseSystemNetHttp();
         options.UseAspNetCore()
             .EnableRedirectionEndpointPassthrough()
